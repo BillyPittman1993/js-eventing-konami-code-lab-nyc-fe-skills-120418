f4c679b9fcaebfc644b1b8e92815cd42fbc2d1c2
BillyPittman1993 <billytrading93@gmail.com>
2019-01-30 00:27:46 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ function init() {
   // Write your JavaScript code inside the init() function
 
  var index = 0;
-  document.addEventListener('keydown', function (e) {
+  document.addEventListener('keydown', e => {
     const key = parseInt(e.detail || e.which);
 
      if (key === code[index]) {
