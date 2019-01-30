aed60cb4fe556cdead77d85f96ca0129e4031597
BillyPittman1993 <billytrading93@gmail.com>
2019-01-30 00:23:23 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -3,8 +3,19 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
 
- document.body.addEventListener('keydown', function(e) {
-  console.log('alkdsjflksdjflksj')
-  console.log(e.which)
-})
-}
+ var index = 0;
+  document.addEventListener('keydown', function (e) {
+    const key = parseInt(e.detail || e.which);
+
+     if (key === code[index]) {
+      index++;
+
+       if (index === code.length - 1) {
+        window.alert("YOU DID IT!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }, false);
+}	
