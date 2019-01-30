cceded5d8092c4e9bb3d2e97ed42badf1654b64d
BillyPittman1993 <billytrading93@gmail.com>
2019-01-30 00:14:02 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,8 +2,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0;
 
-const input = document.body('mocha')
-input.addEventListener('keydown', e => console.log(e.which))
+const mocha = document.body('mocha')
+mocha.addEventListener('keydown', e => console.log(e.which))
 
 function init() {
   // Write your JavaScript code inside the init() function
